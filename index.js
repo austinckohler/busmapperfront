a83d266b7c5cfa991f4915255a78e37460d67993
austinckohler <austinckohler@gmail.com>
2020-05-14 17:15:44 -0600

    build and burned, created a helper method for my fetch request for post, added a button for a delete function

--- index.js
+++ index.js
@@ -3,44 +3,55 @@ fetch("http://localhost:3000/buses")
     .then(displayBusInfo);
     
     function displayBusInfo(buses) {
-    buses.forEach(bus => {
-    const busTitle = document.createElement("h3")
-    busTitle.innerText = bus.tag;
-    document.body.append(busTitle);
-});
+    buses.forEach(bus => {showBus(bus)}
+    )}
 
-const newBusForm = document.querySelector("#new-bus-form")
+    function showBus(bus) {
+        const busTitle = document.createElement("h3")
+        const deleteButton = document.createElement('button')
 
-newBusForm.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const formData = new FormData (newBusForm);
-    const newBusTag = formData.get('tag')
-    const newBusCapacity = formData.get('capacity')
-    const newBusRoute = formData.get('current_route')
-    const newBusDriver = formData.get('driver')
-    const newBus = {bus: {
-        tag: newBusTag,
-        capacity: newBusCapacity,
-        current_route: newBusRoute,
-        driver: newBusDriver
+        busTitle.innerText = bus.tag;
+        deleteButton.innerText = "remove bus"
+        deleteButton.id = bus.id
+
+        deleteButton.addEventListener('click', (event) => removeBus(event, busTitle))
+
+        document.body.append(deleteButton)
+        document.body.append(busTitle);
     }
-};
-newBusForm.reset()
 
-const busTitle = document.createElement("h3")
-busTitle.innerText = newBusTag;
-document.body.append(busTitle);
+    const newBusForm = document.querySelector('#new-bus-form')
 
-fetch ('http://localhost:3000/buses', {
-    method: 'POST',    
-    headers: {
-        'content-type': 'application/json',
-        },
-    body: JSON.stringify(newBus),
+    newBusForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const formData = new FormData(newBusForm)
+        const newBusTag = formData.get("tag")
+        const newBusCapacity = formData.get("capacity")
+        const newBusRoute = formData.get("current_route")
+        const newBusDriver = formData.get("driver")
+        const newBus = {
+                tag: newBusTag,
+                capacity: newBusCapacity,
+                route: newBusRoute,
+                driver: newBusDriver
+        }
+
+        fetch("http://localhost:3000/buses", {
+            method: "POST",
+            headers: {
+                "content-type": "application/json",
+                "accept": "application/json"
+            },
+            body: JSON.stringify(newBus.bus)
+        })
+        showBus(newBus)
     })
-})
-    };
+    newBusForm.reset()
 
-function saveNewBusToDatabase(bus) {
-    
-}
+    // function removeBus(event, bus) {
+    //     event.preventDefault();
+    //     bus.remove()
+        
+    //     fetch
+    // }
+
